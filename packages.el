9649e1e90466c0399b321eff4fb29280ca06b40b
Emma <eagriffin@usf.edu>
2021-02-18 16:43:16 -0500

    Add irony-mode

--- packages.el
+++ packages.el
@@ -20,6 +20,7 @@
 (package! elpy)
 (package! py-autopep8)
 (package! nhexl-mode)
+(package! irony)
 
 ;; To install a package directly from a particular repo, you'll need to specify
 ;; a `:recipe'. You'll find documentation on what `:recipe' accepts here:
